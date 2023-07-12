18bfda17efb927116298b5fb8677cd11a9e93dad
jinzaizhichi <jinzaizhichi9888@gmail.com>
2023-07-12 22:47:54 +0800

    update

--- plugins.lua
+++ plugins.lua
@@ -77,6 +77,39 @@ local plugins = {
       vim.g.crystal_auto_format = 1
     end,
   },
+  {
+    'lianghx-319/ChatGPT.nvim',
+    -- event = "VeryLazy",
+    event = "BufRead",
+    config = function()
+      require("chatgpt").setup({
+      -- async_api_key_cmd = "pass show 2dapi/token", 
+      api_key_cmd = "pass show 2dapi/token",
+      api_host_cmd = "pass show openai/host",
+      -- OPENAI_API_HOST= "pass show openai/host",
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+  },
+    {'Exafunction/codeium.vim',
+      event = "VeryLazy",
+      -- event = "BufRead",
+      config = function ()
+        -- Change '<C-g>' here to any keycode you like.
+        vim.keymap.set('i', '<C-h>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+        vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+        vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+        vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+        vim.g.codeium_api_key = vim.fn.system("pass show codeium/api")
+      end,
+    },
+   {'hashivim/vim-terraform',
+      event = "BufRead",
+    },
 }
 
 return plugins
